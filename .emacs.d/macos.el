17e4d7dda10d759ef9ebc2e175d68696c0b91586
Pedro Brasileiro <pedrobrasileiro@gmail.com>
2011-04-25 13:45:27 -0300

    Adicionando suporte ao color-theme no MacOSX

--- .emacs.d/macos.el
+++ .emacs.d/macos.el
@@ -1,9 +1,9 @@
 ;; Configurações específicas no Mac OS X
 
-;; (require 'color-theme)
-;; (load-file "~/.emacs.d/themes/twilight.el")
+(require 'color-theme)
+(load-file "~/.emacs.d/themes/twilight.el")
 
-;; (color-theme-twilight)
+(color-theme-twilight)
 
 (custom-set-faces
   '(default ((t (:inherit nil :stipple nil :height 120 :width normal)))))
